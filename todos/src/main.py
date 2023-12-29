1446af75d6c5d17dd48694240aa811a98b9aa9ad
sososo0 <sh0000@pusan.ac.kr>
2023-12-29 20:56:39 +0900

    feat: Add PATCH todo api of is_done

--- todos/src/main.py
+++ todos/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -51,3 +51,15 @@ class CreateToDoRequest(BaseModel):
 def create_todo_handler(request: CreateToDoRequest):
     todo_data[request.id] = request.dict()
     return todo_data[request.id]
+
+
+@app.patch("/todos/{todo_id}")
+def update_todo_handler(
+        todo_id: int,
+        is_done: bool = Body(..., embed=True),
+    ):
+    todo = todo_data.get(todo_id)
+    if todo:
+        todo["is_done"] = is_done
+        return todo
+    return {}
