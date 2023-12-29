6272c81136f5829366a7b5adb3701e3e9cc9a8e9
sososo0 <sh0000@pusan.ac.kr>
2023-12-29 20:00:15 +0900

    fea: Add GET todos items

--- todos/src/main.py
+++ todos/src/main.py
@@ -6,3 +6,27 @@ app = FastAPI()
 @app.get("/")
 def health_check_handler():
     return {"ping": "pong"}
+
+
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "실전! FastAPI 섹션 0 수강",
+        "is_done": True
+    },
+    2: {
+        "id": 2,
+        "contents": "실전! FastAPI 섹션 1 수강",
+        "is_done": False
+    },
+    3: {
+        "id": 3,
+        "contents": "실전! FastAPI 섹션 2 수강",
+        "is_done": False
+    }
+}
+
+
+@app.get("/todos")
+def get_todos_handler():
+    return list(todo_data.values())
