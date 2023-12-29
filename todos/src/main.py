373c34e7221dce2430bf1423e0a3a88f9d2b2dc5
sososo0 <sh0000@pusan.ac.kr>
2023-12-29 20:04:50 +0900

    feat: Add query paramter in GET todos items

--- todos/src/main.py
+++ todos/src/main.py
@@ -28,5 +28,8 @@ todo_data = {
 
 
 @app.get("/todos")
-def get_todos_handler():
-    return list(todo_data.values())
+def get_todos_handler(order: str):
+    ret = list(todo_data.values())
+    if order == "DESC":
+        return ret[::-1]
+    return ret
