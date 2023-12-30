ad11520422cdad2119f87ce9c7d5f95b811f3769
sososo0 <sh0000@pusan.ac.kr>
2023-12-30 23:02:56 +0900

    feat: Add connect to database file use sqlalchemy

---
+++ todos/src/database/connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3307/todos"
+
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
