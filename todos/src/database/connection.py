77d96694d70885b30d6b8ce194cc79cdb1f34ab2
sososo0 <sh0000@pusan.ac.kr>
2023-12-31 21:33:46 +0900

    feat: Add generator in database connection to ãhandle session

--- todos/src/database/connection.py
+++ todos/src/database/connection.py
@@ -6,3 +6,11 @@ DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3307/todos"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    session = SessionFactory()
+    try:
+        yield session
+    finally:
+        session.close()
