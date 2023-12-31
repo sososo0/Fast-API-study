d2de1464d522fc7164a555f65d5ac0617e46a242
sososo0 <sh0000@pusan.ac.kr>
2023-12-31 22:22:27 +0900

    fix: Change response to HTTP Response form in GET todos api

--- todos/src/main.py
+++ todos/src/main.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 
 from database.connection import get_db
 from database.repository import get_todos
+from schema.response import ToDoSchema
+from schema.response import ListToDoResponse
 
 app = FastAPI()
 
@@ -36,11 +38,15 @@ todo_data = {
 def get_todos_handler(
     order: str | None = None,
     session: Session = Depends(get_db),
-):
+) -> ListToDoResponse:
     todos: List[ToDo] = get_todos(session=session)
     if order and order == "DESC":
-        return todos[::-1]
-    return todos
+        return ListToDoResponse(
+            todos = [ToDoSchema.from_orm(todos) for todo in todos[::-1]]
+        )
+    return ListToDoResponse(
+        todos = [ToDoSchema.from_orm(todos) for todo in todos]
+    )
 
 
 @app.get("/todos/{todo_id}", status_code=200)
