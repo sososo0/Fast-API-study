853ea37b4a94be7283096ff6e16dc14814791def
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 14:51:21 +0900

    feat: Add single GET ToDo func in repository

--- todos/src/database/repository.py
+++ todos/src/database/repository.py
@@ -8,3 +8,6 @@ from database.orm import ToDo
 
 def get_todos(session: Session) -> List[ToDo]:
     return list(session.scalars(select(ToDo)))
+
+def get_todo_by_todo_id(session: Session, todo_id: int) -> ToDo | None:
+    return session.scalar(select(ToDo).where(ToDo.id == todo_id))
