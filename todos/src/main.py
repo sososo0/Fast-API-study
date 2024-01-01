3bef7b8c0998c5d9745e2821ada14fa74759679b
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 15:10:23 +0900

    fix: Apply ORM in single GET ToDo API

--- todos/src/main.py
+++ todos/src/main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from database.connection import get_db
 from database.repository import get_todos
+from database.repository import get_todo_by_todo_id
 from schema.response import ToDoSchema
 from schema.response import ListToDoResponse
 
@@ -50,10 +51,13 @@ def get_todos_handler(
 
 
 @app.get("/todos/{todo_id}", status_code=200)
-def get_todo_handler(todo_id: int):
-    todo = todo_data.get(todo_id)
+def get_todo_handler(
+    todo_id: int,
+    session: Session = Depends(get_db),
+):
+    todo: ToDo | None = get_todo_by_todo_id(session=session, todo_id=todo_id)
     if todo:
-        return todo
+        return ToDoSchema.from_orm(todo)
     raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 
