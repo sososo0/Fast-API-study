fc9e582dfed6c7c8c02d6d2e0e43cf3af4fe3513
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 17:42:59 +0900

    fix: Remove temporary todo data

--- todos/src/main.py
+++ todos/src/main.py
@@ -20,25 +20,6 @@ def health_check_handler():
     return {"ping": "pong"}
 
 
-todo_data = {
-    1: {
-        "id": 1,
-        "contents": "실전! FastAPI 섹션 0 수강",
-        "is_done": True
-    },
-    2: {
-        "id": 2,
-        "contents": "실전! FastAPI 섹션 1 수강",
-        "is_done": False
-    },
-    3: {
-        "id": 3,
-        "contents": "실전! FastAPI 섹션 2 수강",
-        "is_done": False
-    }
-}
-
-
 @app.get("/todos", status_code=200)
 def get_todos_handler(
     order: str | None = None,
