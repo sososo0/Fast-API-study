604894d633b1277048394c0eba05964807ef4e2e
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 18:12:38 +0900

    test: Add GET todos API in main test

--- todos/src/tests/test_main.py
+++ todos/src/tests/test_main.py
@@ -10,3 +10,27 @@ def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
+
+
+def test_get_todos():
+    # order = ASC
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
+            {"id": 3, "contents": "FastAPI Section 2", "is_done": True}
+        ]
+    }
+
+    # order = DESC
+    response = client.get("/todos?order=DESC")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 3, "contents": "FastAPI Section 2", "is_done": True},
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
+            {"id": 1, "contents": "FastAPI Section 0", "is_done": True}
+        ]
+    }
