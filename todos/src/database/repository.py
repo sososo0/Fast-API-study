6006426fca2f2b9a2ad44fd475cc2de1e15c2244
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 19:46:05 +0900

    fix: Change repository from defs to one integrated ToDoRepository class

--- todos/src/database/repository.py
+++ todos/src/database/repository.py
@@ -6,28 +6,28 @@ from sqlalchemy.orm import Session
 from database.orm import ToDo
 
 
-def get_todos(session: Session) -> List[ToDo]:
-    return list(session.scalars(select(ToDo)))
+class ToDoRepository:
+    def __init__(self, session: Session):
+        self.session = session
 
+    def get_todos(self) -> List[ToDo]:
+        return list(self.session.scalars(select(ToDo)))
 
-def get_todo_by_todo_id(session: Session, todo_id: int) -> ToDo | None:
-    return session.scalar(select(ToDo).where(ToDo.id == todo_id))
+    def get_todo_by_todo_id(self, todo_id: int) -> ToDo | None:
+        return self.session.scalar(select(ToDo).where(ToDo.id == todo_id))
 
+    def create_todo(self, todo: ToDo) -> ToDo:
+        self.session.add(instance=todo)
+        self.session.commit() # db save
+        self.session.refresh(instance=todo)
+        return todo
 
-def create_todo(session: Session, todo: ToDo) -> ToDo:
-    session.add(instance=todo)
-    session.commit() # db save
-    session.refresh(instance=todo)
-    return todo
+    def update_todo(self, todo: ToDo) -> ToDo:
+        self.session.add(instance=todo)
+        self.session.commit() # db save
+        self.session.refresh(instance=todo)
+        return todo
 
-
-def update_todo(session: Session, todo: ToDo) -> ToDo:
-    session.add(instance=todo)
-    session.commit() # db save
-    session.refresh(instance=todo)
-    return todo
-
-
-def delete_todo(session: Session, todo_id: int) -> None:
-    session.execute(delete(ToDo).where(ToDo.id == todo_id))
-    session.commit()
+    def delete_todo(self, todo_id: int) -> None:
+        self.session.execute(delete(ToDo).where(ToDo.id == todo_id))
+        self.session.commit()
