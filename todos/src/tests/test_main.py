2b0c370250b357263aa580600e86ae8718bdeb31
sososo0 <sh0000@pusan.ac.kr>
2024-01-01 18:56:01 +0900

    test: Add POST todo API test code with mock and spy

--- todos/src/tests/test_main.py
+++ todos/src/tests/test_main.py
@@ -53,3 +53,26 @@ def test_get_todo(client, mocker):
     response = client.get("/todos/1")
     assert response.status_code == 404
     assert response.json() == {"detail": "ToDo Not Found"}
+
+
+def test_create_todo(client, mocker):
+    create_spy = mocker.spy(ToDo, "create")
+    mocker.patch(
+        "main.create_todo",
+        return_value=ToDo(id=1, contents="todo", is_done=True)
+    )
+
+    body = {
+        "contents": "test",
+        "is_done": False
+    }
+    response = client.post("/todos", json=body)
+
+    # spy로 request body 검증 -> post에서 request에 대한 create 부분을 검증하는 것
+    assert create_spy.spy_return.id is None
+    assert create_spy.spy_return.contents == "test"
+    assert create_spy.spy_return.is_done is False
+
+    # mocking한 부분 검증 -> mocking된 data로 응답을 하게 된다.
+    assert response.status_code == 201
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
