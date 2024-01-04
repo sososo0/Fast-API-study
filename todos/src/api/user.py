4e6e735b7b783038405fcfc0d3da7ebf6d343c98
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 13:06:07 +0900

    test: Add user sign up test code
    - mocking을 이용해서 hashing 사용
    - assert_called_once_with를 사용해서 어떤 인자가 함수를 호출 했는지 검증
    - status code와 json 응답을 검증
    - sign up api에서 request 모듈 및 password 오타 수정

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from schema.request import SignUpRequeest
+from schema.request import SignUpRequest
 from schema.response import UserSchema
 from service.user import UserService
 from database.repository import UserRepository
@@ -11,16 +11,16 @@ router = APIRouter(prefix="/users")
 
 @router.post("/sign-up", status_code=201)
 def user_sign_up_handler(
-    request: SignupRequest,
+    request: SignUpRequest,
     user_service: UserService = Depends(),
     user_repo: UserRepository = Depends(),
 ):
     hashed_password: str = user_service.hash_password(
         plain_password=request.password
     )
-    user: User = UserService.create(
+    user: User = User.create(
         username=request.username,
-        hashed_password =request.hashed_password
+        hashed_password =hashed_password
     )
     user: User = user_repo.save_user(user=user) # id=int
     return UserSchema.from_orm(user)
