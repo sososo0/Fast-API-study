e0fb775c17664f29a28437c67ccaf28d279e5ff4
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 11:24:14 +0900

    feat: Add password hashing process in user sign up api

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from schema.request import SignUpRequeest
+from service.user import UserService
 
 router = APIRouter(prefix="/users")
 
 
 @router.post("/sign-up", status_code=201)
-def user_sign_up_handler(request: SignupRequest):
+def user_sign_up_handler(
+    request: SignupRequest,
+    user_service: UserService = Depends(),
+):
     # 1. request body로 username과 password 받기 - 우선은 중복체크는 하지 않을 거임
     # 2. password를 hashing을 통해 암호화하기 - hashed_password를 통해 암호화하기
+    hashed_password: str = user_service.hash_password(
+        plain_password=request.password
+    )
+
     # 3. User(username, hashed_password)를 통해 User 생성
     # 4. 생성해준 User를 db에 저장
     # 5. user response를 응답해주기 - return user(id, username)
