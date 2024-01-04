0eb774b24d10d21ca6d6d559fc1b09ed246ac700
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 10:52:57 +0900

    feat: Add user in app router
    - user 파일에 있는 app router를 지우고 main에 추가

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
-from api import todo, user
-
 router = APIRouter(prefix="/users")
-app.include_router(todo.router)
-app.include_router(user.router)
 
 
 @router.post("/sign-up")
--- todos/src/main.py
+++ todos/src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from api import todo
+from api import todo, user
 
 app = FastAPI()
 app.include_router(todo.router)
+app.include_router(user.router)
 
 
 @app.get("/")
