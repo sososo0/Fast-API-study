6c57936547316c4cbe82ce62634db6382eafce28
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 13:35:29 +0900

    feat: Add user login func frame and comment to create login api

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -24,3 +24,13 @@ def user_sign_up_handler(
     )
     user: User = user_repo.save_user(user=user) # id=int
     return UserSchema.from_orm(user)
+
+
+@router.post("log-in")
+def user_log_in_handler():
+    # 1. request body로 username과 password 입력 받기
+    # 2. database에서 username을 통해 user를 읽는다.
+    # 3. user의 password(해싱된 pw)와 입력으로 받은 request의 password(일반 pw)가 같은지 확인
+    # -> bcrypt.checkpw를 통해 검증
+    # 4. 유효한 user면 jwt를 생성해서 return 하기
+    return True
