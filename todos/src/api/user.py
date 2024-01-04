218d05f7c6ad63debc335970a938c8f84ea5c890
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 11:08:09 +0900

    fix: Add parameter to handle request body

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -4,7 +4,7 @@ router = APIRouter(prefix="/users")
 
 
 @router.post("/sign-up", status_code=201)
-def user_sign_up_handler():
+def user_sign_up_handler(request: SignupRequest):
     # 1. request body로 username과 password 받기 - 우선은 중복체크는 하지 않을 거임
     # 2. password를 hashing을 통해 암호화하기 - hashed_password를 통해 암호화하기
     # 3. User(username, hashed_password)를 통해 User 생성
