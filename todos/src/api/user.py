08d46d58ce0e6100c97976be391cd360f50ce3e2
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 11:37:50 +0900

    feat: Add save user code to use repository

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 
 from schema.request import SignUpRequeest
 from service.user import UserService
+from database.repository import UserRepository
 from database.orm import User
 
 router = APIRouter(prefix="/users")
@@ -11,6 +12,7 @@ router = APIRouter(prefix="/users")
 def user_sign_up_handler(
     request: SignupRequest,
     user_service: UserService = Depends(),
+    user_repo: UserRepository = Depends(),
 ):
     # 1. request body로 username과 password 받기 - 우선은 중복체크는 하지 않을 거임
     # 2. password를 hashing을 통해 암호화하기 - hashed_password를 통해 암호화하기
@@ -25,5 +27,7 @@ def user_sign_up_handler(
     )
 
     # 4. 생성해준 User를 db에 저장
+    user: User = user_repo.save_user(user=user) # id=int
+
     # 5. user response를 응답해주기 - return user(id, username)
     return True
