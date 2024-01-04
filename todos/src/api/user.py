997df4b777e1579add19c87caaac6fd3b9882f34
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 13:44:21 +0900

    feat: Get user through repository and add exception handling

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from schema.request import SignUpRequest, LogInRequest
 from schema.response import UserSchema
@@ -29,9 +29,16 @@ def user_sign_up_handler(
 @router.post("log-in")
 def user_log_in_handler(
     request: LoginRequest,
+    user_repo: UserRepository = Depends(),
 ):
     # 1. request body로 username과 password 입력 받기
     # 2. database에서 username을 통해 user를 읽는다.
+    user: User | None = user_repo.get_user_by_username(
+        username=request.username
+    )
+    if not user:
+        raise HTTPException(status_code=404, detail="User Not Found")
+
     # 3. user의 password(해싱된 pw)와 입력으로 받은 request의 password(일반 pw)가 같은지 확인
     # -> bcrypt.checkpw를 통해 검증
     # 4. 유효한 user면 jwt를 생성해서 return 하기
--- todos/src/database/repository.py
+++ todos/src/database/repository.py
@@ -39,6 +39,13 @@ class UserRepository:
     def __init__(self, session: Session = Depends(get_db)):
         self.session = session
 
+
+    def get_user_by_username(self, username: str) -> User | None:
+        return self.session.scalar(
+            select(User).where(User.username == username)
+        )
+
+
     def save_user(self, user: User) -> User:
         self.session.add(instance=user)
         self.session.commit()  # db save
