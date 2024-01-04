ddec0efa235788e02910e4420915c7b051b10fc6
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 10:42:20 +0900

    test: Divide test code into main and todos api
    - 기존의 test 코드를 main과 todos api test 코드로 분리
    - 기존에 main에 있던 health check 부분을 새로 만든 main test로 옮김

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ E
 todos/src/api/user.py,1\1\11972e0cca8cf2e9c5e9155ce18ceeb8313e6af8
 A
 todos/src/main.py,7\0\700c97400c7bfa9adfd913cbcb02e5724b7e4bb6
+L
+todos/src/tests/test_main.py,d\8\d8dca9fb8f1c3fdc3c7fecf2e8cc837bddddd403
+Q
+!todos/src/tests/test_todos_api.py,d\5\d52a348933882dacd871338356ad4f316b887aa1
---
+++ todos/src/tests/test_todos_api.py
@@ -0,0 +1,141 @@
+from database.orm import ToDo
+from database.repository import ToDoRepository
+
+
+def test_get_todos(client, mocker):
+    # order = ASC
+    mocker.patch.object(ToDoRepository, "get_todos", return_value=[
+        ToDo(id=1, contents="FastAPI Section 0", is_done=True),
+        ToDo(id=2, contents="FastAPI Section 1", is_done=False)
+    ])
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False}
+        ]
+    }
+
+    # order = DESC
+    response = client.get("/todos?order=DESC")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
+            {"id": 1, "contents": "FastAPI Section 0", "is_done": True}
+        ]
+    }
+
+
+def test_get_todo(client, mocker):
+    # 200
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=ToDo(id=1, contents="todo", is_done=True)
+    )
+
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
+
+    # 404
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=None
+    )
+
+    response = client.get("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "ToDo Not Found"}
+
+
+def test_create_todo(client, mocker):
+    create_spy = mocker.spy(ToDo, "create")
+    mocker.patch.object(
+        ToDoRepository,
+        "create_todo",
+        return_value=ToDo(id=1, contents="todo", is_done=True)
+    )
+
+    body = {
+        "contents": "test",
+        "is_done": False
+    }
+    response = client.post("/todos", json=body)
+
+    # spy로 request body 검증 -> post에서 request에 대한 create 부분을 검증하는 것
+    assert create_spy.spy_return.id is None
+    assert create_spy.spy_return.contents == "test"
+    assert create_spy.spy_return.is_done is False
+
+    # mocking한 부분 검증 -> mocking된 data로 응답을 하게 된다.
+    assert response.status_code == 201
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
+
+
+def test_update_todo(client, mocker):
+    # 200
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=ToDo(id=1, contents="todo", is_done=True)
+    )
+
+    # 올바른 데이터가 mocking 되었는지 확인하기 위함
+    undone = mocker.patch.object(ToDo, "undone")
+
+    mocker.patch.object(
+        ToDoRepository,
+        "update_todo",
+        return_value=ToDo(id=1, contents="todo", is_done=False)
+    )
+
+    response = client.patch("/todos/1", json={"is_done": False})
+
+    # undone이 호출되었는지 확인
+    undone.assert_called_once_with()
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": False}
+
+    # 404
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=None
+    )
+
+    response = client.patch("/todos/1", json={"is_done": True})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "ToDo Not Found"}
+
+
+def test_delete_todo(client, mocker):
+    # 204
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=ToDo(id=1, contents="todo", is_done=True)
+    )
+
+    mocker.patch.object(
+        ToDoRepository,
+        "delete_todo", return_value=None
+    )
+
+    response = client.delete("/todos/1")
+    assert response.status_code == 204
+
+    # 404
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=None
+    )
+
+    response = client.delete("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "ToDo Not Found"}
