824682c188ee70ab70415f13c0a527cde5cb5c58
sososo0 <sh0000@pusan.ac.kr>
2024-01-04 10:16:10 +0900

    fix: Add todos relationship to User class in database orm

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+I
+todos/src/database/orm.py,3\2\32b92cace46c62232a654491e716be3f80ce51e9
--- todos/src/database/orm.py
+++ todos/src/database/orm.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 from schema.request import CreateToDoRequest
 
@@ -37,3 +37,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(256), nullable=False)
     password = Column(String(256), nullable=False)
+    todos = relationship("ToDo", lazy="joined")
