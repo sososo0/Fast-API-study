4e43c9cdc2debc0e3ba361dd61244f54ae02b604
sososo0 <sh0000@pusan.ac.kr>
2024-01-06 12:05:43 +0900

    remove: Remove api logic comment in user login api

--- todos/src/api/user.py
+++ todos/src/api/user.py
@@ -32,16 +32,12 @@ def user_log_in_handler(
     user_service: UserService = Depends(),
     user_repo: UserRepository = Depends(),
 ):
-    # 1. request body로 username과 password 입력 받기
-    # 2. database에서 username을 통해 user를 읽는다.
     user: User | None = user_repo.get_user_by_username(
         username=request.username
     )
     if not user:
         raise HTTPException(status_code=404, detail="User Not Found")
 
-    # 3. user의 password(해싱된 pw)와 입력으로 받은 request의 password(일반 pw)가 같은지 확인
-    # -> bcrypt.checkpw를 통해 검증
     verified: bool = user_service.verify_password(
         plain_password=request.password,
         hashed_password=user.password,
@@ -49,7 +45,5 @@ def user_log_in_handler(
     if not verified:
         raise HTTPException(status_code=401, detail="Not Authorized")
 
-    # 4. 유효한 user면 jwt를 생성해서 return 하기
     access_toekn: str = user_service.create_jwt(username=user.username)
-
     return JWTResponse(access_toekn=access_toekn)
