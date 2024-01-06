fedb403f888c1c26f07b2a4e7d80a99e8003d327
sososo0 <sh0000@pusan.ac.kr>
2024-01-06 12:00:14 +0900

    feat: Add creat jwt in user service

--- todos/src/service/user.py
+++ todos/src/service/user.py
@@ -1,8 +1,11 @@
 import bcrypt
-
+from datetime import datetime, timedelta
+from jose import jwt
 
 class UserService:
     encoding: str = "UTF-8"
+    secret_key: str = "65e759cc2e7d75d44c1ffa263bbee80f7974a7e3aadb9695b29a3a61a999726f"
+    jwt_algorithm:str = "HS256"
 
     def hash_password(self, plain_password: str) -> str:
         hashed_password: bytes = bcrypt.hashpw(
@@ -18,4 +21,15 @@ class UserService:
         return bcrypt.checkpw(
             plain_password.encode(self.encoding),
             hashed_password.encode(self.encoding)
+        )
+
+
+    def create_jwt(self, username: str) -> str:
+        return jwt.encode(
+            {
+                "sub": username,
+                "exp": datetime.now() + timedelta(days=1),
+            },
+            self.secret_key,
+            algorithm=self.jwt_algorithm,
         )
